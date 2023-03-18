b74a68ffdd754806f99d4d856578020acaaa8b91
HeatherMarie2 <77646633+Heather-Marie@users.noreply.github.com>
2023-03-18 18:05:52 +0000

    shelter.html

--- replit.nix
+++ replit.nix
@@ -1,5 +1,7 @@
 { pkgs }: {
   deps = [
+    pkgs.sudo
+    pkgs.sudo
     pkgs.pip install flask
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server  
